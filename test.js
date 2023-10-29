2fbcc77e37c130fb295d6eed6ff86eb86dcaa9a2
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-10-30 00:05:02 +0600

    onclick eliment used JavaScript

--- test.js
+++ test.js
@@ -3,3 +3,15 @@ console.log ("this is my learning parts javascript")
 alert("this high risk but this jocks ")
 document.write("This is JavaScript text </br>")
 document.write("this my home practic parts JavaScript")
+function upDate() {
+    document.getElementById("demo").innerHTML = "Click JavaScript code ?";
+}
+function Time() {
+    document.getElementById("somoi").innerHTML = Date();
+} 
+function toer() {
+    document.getElementById("real").innerHTML = "My Name Is Shafiqul Islam Sagor";
+} 
+function house() {
+    document.getElementById("bari").innerHTML = "My Home Distric Sherpur"
+}
